de1abd502a32ae5571aec3045287ef7b62018129
impaulinha <anaaguiar20016@gmail.com>
2025-09-22 13:39:15 -0300

    chore: prepare for history cleanup

---
+++ app/android/app/.cxx/RelWithDebInfo/19154m2j/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/anaag/.gradle/caches/8.14.3/transforms/7877f66ae4a9780f23a0fae645374ca8/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/anaag/.gradle/caches/8.14.3/transforms/7877f66ae4a9780f23a0fae645374ca8/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ app/android/app/.cxx/RelWithDebInfo/19154m2j/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "C:/Users/anaag/.gradle/caches/8.14.3/transforms/d1b93ea6002a5dd45ea11e44a3168480/transformed/jetified-hermes-android-0.81.4-release/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/anaag/.gradle/caches/8.14.3/transforms/d1b93ea6002a5dd45ea11e44a3168480/transformed/jetified-hermes-android-0.81.4-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
